862b23e4f8ede5a959d948cc485de601fc8c0e24
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-08-05 17:52:58 +0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+list : https://monosnap.com/file/B0yE308jA8vvsjPC9GgX6XXBgi7HwE
+
+get : https://monosnap.com/file/0n1lRqXD2cnlXvqYUpsdt0ZruqKZ3C
+
+add : https://monosnap.com/file/nUl0aBEZ5JXUZRHWHJsH83rwo5YDmT
+
+remove : https://monosnap.com/file/jVBYfEEtDZOchDCinAemdbW5jDqjus
---
+++ contacts.js
@@ -0,0 +1,45 @@
+const { program } = require("commander");
+
+const contacts = require("./db");
+
+const invokeAction = async ({ action, id, phone, email, name }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      return console.log(removedContact);
+
+    default:
+      console.warn("unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+invokeAction(options);
+
+// {
+//   name: "Jimmy Miller",
+//   email: "jimmy.ante@vestibul.co.uk",
+//   phone: "(997) 977-4592",
+// }
